af7a9a0e12b8aa782053da76225a5b7956f4c62a
Hasan Ozovali <ozovalihasan@gmail.com>
2020-10-20 12:32:24 +0300

    Add feature to show and hide elements

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-    
+function makeVisible(event) {
+  updateVisibility(event, 'visible');
+}
+
+function makeHidden(event) {
+  updateVisibility(event, 'hidden');
+}
+
+function updateVisibility(element, visibility) {
+  const seeProject = element.getElementsByClassName('see-project');
+  seeProject[0].style.visibility = visibility;
+  const programs = element.getElementsByClassName('programs');
+  programs[0].style.visibility = visibility;
+}
+
+const projects = document.getElementsByClassName('project');
+
+for (let project of projects) {
+  project.addEventListener('mouseenter', function (event) {
+    makeVisible(event.target);
+  });
+  project.addEventListener('mouseleave', function (event) {
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      makeHidden(event.target);
+    }
+  });
+}
+
+window.addEventListener('resize', function (params) {
+  for (let project of projects) {
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      makeHidden(project);
+    } else {
+      makeVisible(project);
+    }
+  }
+});
