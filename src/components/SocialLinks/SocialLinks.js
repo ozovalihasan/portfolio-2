4221913d9a57a3ca6b1d61e1534dfc9686b55b09
Hasan Ozovali <ozovalihasan@gmail.com>
2021-02-10 02:14:06 +0300

    Update SocialLinks by adding styles

--- src/components/SocialLinks/SocialLinks.js
+++ src/components/SocialLinks/SocialLinks.js
@@ -1,21 +1,39 @@
 import React, { useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { styled } from '@linaria/react';
+import PropTypes from 'prop-types';
 import store from '../store';
 
-const SocialLinks = () => {
+const SocialLinks = ({ hoverColor }) => {
   const { socialLinks } = useContext(store);
   return (
-    <div>
+    <SocialLinksOuter>
       {socialLinks.map(socialLink => (
-        <div key={socialLink.name}>
-          {socialLink.name}
-          <div>
-            <FontAwesomeIcon icon={socialLink.icon} />
-          </div>
-        </div>
+        <Link target="blank" title={socialLink.name} href={socialLink.link} key={socialLink.name} hoverColor={hoverColor}>
+          <UpdatedFontAwesomeIcon icon={socialLink.icon} />
+        </Link>
       ))}
-    </div>
+    </SocialLinksOuter>
   );
 };
 
+const SocialLinksOuter = styled.div`
+  display: flex;
+`;
+
+const Link = styled.a`
+  &:hover {
+    color: ${props => props.hoverColor}
+  }
+`;
+
+const UpdatedFontAwesomeIcon = styled(FontAwesomeIcon)`
+  font-size: 40px;
+  margin: 10px;
+  
+`;
+
+SocialLinks.propTypes = {
+  hoverColor: PropTypes.string.isRequired,
+};
 export default SocialLinks;
