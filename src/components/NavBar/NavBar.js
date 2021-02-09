dcc8bd7512aa9cff1041fdbcd99ecc25cebb0418
Hasan Ozovali <ozovalihasan@gmail.com>
2021-02-10 02:13:00 +0300

    Update style of NavBar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import { styled } from '@linaria/react';
 import React from 'react';
+import * as color from '../styleSheets/styleVariables';
 
 const NavBar = () => {
   const navBar = [
@@ -18,24 +19,68 @@ const NavBar = () => {
   ];
 
   return (
-    <div>
-      <DeveloperImage src="assets/hasanozovali.jpg" />
-      {navBar.map(oneTab => (
-        <div key={oneTab.name}>
-          <a href={oneTab.link} title={oneTab.name}>
-            {oneTab.name}
-          </a>
-        </div>
-      ))}
+    <NavBarOuter>
+      <NavBarInner>
 
-    </div>
+        <DeveloperImage src="assets/hasanozovali.jpg" />
+        {navBar.map(oneTab => (
+          <NavBarItem href={oneTab.link} title={oneTab.name} key={oneTab.name}>
+            {oneTab.name}
+          </NavBarItem>
+        ))}
+
+      </NavBarInner>
+    </NavBarOuter>
   );
 };
 
+const NavBarOuter = styled.div`
+  flex-shrink: 0;
+  width: 100%;
+  position: fixed;
+  z-index: 10;
+  background-color: ${color.fifthColor};
+  box-shadow: 0 0 10px ${color.fourthColor};
+
+  @media screen and (min-width: 768px) {
+    position: static;
+    width: 20%;
+    box-shadow: none;
+  }
+`;
+
+const NavBarInner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+
+  @media screen and (min-width: 768px) {
+    position: fixed;
+    width: 20%;
+    flex-direction: column;
+    justify-content: start;
+  }
+`;
+
 const DeveloperImage = styled.img`
   background-size: cover;
-  width: 50px;
-  height: 50px; 
+  width: 75px;
+  border-radius: 50rem;
+  margin: 20px;
+`;
+
+const NavBarItem = styled.a`
+  width: 100%;
+  text-align: center;
+  margin: 10px;
+  padding: 10px;
+  color: ${color.thirdColor}
+  border-radius: 50rem;
+
+  &:hover {
+    background-color: ${color.sixthColor}
+  }
 `;
 
 export default NavBar;
