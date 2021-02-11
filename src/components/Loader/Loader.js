982c11ed8410760e7d4a6d2dbfbc32cf7a0bf476
Hasan Ozovali <ozovalihasan@gmail.com>
2021-02-11 13:55:20 +0300

    Update Loader by adding background color

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,10 +1,22 @@
 import { styled } from '@linaria/react';
 import React from 'react';
-import * as color from '../styleVariables';
+import * as color from '../styleSheets/styleVariables';
 
 const Loader = () => (
-  <MainLoading />
+  <LoaderOuter>
+    <MainLoading />
+  </LoaderOuter>
 );
+
+const LoaderOuter = styled.div`
+  background-color: ${color.fifthColor};
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 20;
+`;
 
 const MainLoading = styled.div`
     position: absolute;
