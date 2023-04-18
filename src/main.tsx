21cb383f8a7841d35b9ae70e3f6d80b2684252ab
ozovalihasan <ozovalihasan@gmail.com>
2023-04-18 12:31:06 +0300

    Refactor the main page

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client'
 import "./main.css"
 import App from './components/App/App';
 
-
-const Base = () => {
-  
-
-  return (
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-}
-
+const Base = () => (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Base></Base>,
