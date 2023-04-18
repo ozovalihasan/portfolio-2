5abab6ef6d642aaff552ceec17595b50c69d1446
ozovalihasan <ozovalihasan@gmail.com>
2023-04-18 12:19:46 +0300

    Update routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Route, redirect, Routes } from "react-router-dom";
-import store from './components/store';
+import { BrowserRouter as Router, Route, redirect, Routes, useLocation } from "react-router-dom";
+import { routes } from './components/store';
 import App from './components/App/App'
 import "./main.css"
 
@@ -9,17 +9,31 @@ const redirectTo = async (url: string) => {
   window.location.href = url;
 }
 
-const RedirectedLiveLink = ({url}: {url: string}) => {
+const RedirectedLiveLink = () => {
+  let { pathname } = useLocation();
+  
+  pathname = pathname.replace(/^\//, "")
+  const route = routes[pathname as ( (keyof typeof routes))]
+
   setTimeout(() => {
-    redirectTo(url)
+    redirectTo(route || "/" )
   }, 2000);
 
   return <div className='w-screen h-screen text-center flex flex-col justify-center items-center text-fifth bg-third'>
     <div>
-      Thanks for checking this project.
+      {
+        route
+          ? "Thanks for checking this project."
+          : "The page is not found."
+      }
     </div>
     <div className='w-3/4 pt-8'>
-      You are being redirected to the live demo of the project in a short time.
+    {
+      route
+        ? "You are being redirected to the related page in a short time." 
+        : "You are being redirected to the main page."
+    }
+      
     </div>
 
     <div className='animate-ping h-8 w-8 bg-first rounded-full mt-16'>
@@ -29,7 +43,7 @@ const RedirectedLiveLink = ({url}: {url: string}) => {
 }
 
 const Base = () => {
-  const { projects } = useContext(store);
+  
 
   return (
     <React.StrictMode>
@@ -37,29 +51,12 @@ const Base = () => {
         <Routes>
 
           <Route 
-            path='/ead' 
+            path='/*' 
             element={
-              <RedirectedLiveLink url={projects.find(({name}) => name === "EAD")?.liveLink || ""} />
+              <RedirectedLiveLink />
             } 
           />
-          <Route 
-            path='/dr-ti' 
-            element={
-              <RedirectedLiveLink url={projects.find(({name}) => name === 'Dr. Ti')?.liveLink || ""} />
-            } 
-          />
-          <Route 
-            path='/whistle' 
-            element={
-              <RedirectedLiveLink url={projects.find(({name}) => name === 'Whistle')?.liveLink || ""} />
-            } 
-          />
-          <Route 
-            path='/escape' 
-            element={
-              <RedirectedLiveLink url={projects.find(({name}) => name === 'Escape')?.liveLink || ""} />
-            } 
-          />
+          
           <Route path='/' element={<App />}/>
         </Routes>
       </Router>
